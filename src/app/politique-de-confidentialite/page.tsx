cc7a352cec7177357053d9284f86961a00661976
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-30 13:22:36 +0100

    Update page.tsx

--- src/app/politique-de-confidentialite/page.tsx
+++ src/app/politique-de-confidentialite/page.tsx
@@ -8,7 +8,7 @@ const page = () => {
             <NavBar /> 
             <main className='mx-auto flex max-w-5xl items-center justify-between flex-col p-6 lg:px-8' >
                 <h1 className="flex flex-col mx-auto py-10 text-center lg:text-5xl text-2xl lg:w-11/12 mt-10 lg:mt-3 text-gray-700">
-        trouver-mon-chantier.com <br />
+        nicolascastera.fr <br />
         Politique de confidentialité
       </h1>
       <div className="text-gray-700">
@@ -16,7 +16,7 @@ const page = () => {
             1. Collecte des Informations
           </h3>
           <p className="text-gray-500 text-xs md:text-base font-light w-6/6 mt-5">
-            Lors de votre navigation sur trouver-mon-chantier.com, nous recueillons des
+            Lors de votre navigation sur nicolascastera.fr, nous recueillons des
             données relatives à votre bien afin d'en estimer sa valeur. Ces
             informations incluent la localisation, la taille, le type de bien,
             et d'autres caractéristiques pertinentes. Par ailleurs, des données
@@ -31,7 +31,7 @@ const page = () => {
             Vos données, notamment celles concernant votre bien immobilier, sont
             essentielles pour nous permettre d'effectuer une estimation. Elles
             peuvent également être utilisées pour vous présenter des offres ou
-            services pertinents. trouver-mon-chantier.com peut choisir de revendre ces
+            services pertinents. nicolascastera.fr peut choisir de revendre ces
             données à des agents ou agences immobilières pour vous proposer des
             services adaptés. Cependant, vous serez toujours informé et aurez la
             possibilité de refuser tout partage d'informations avec des tiers.
@@ -40,7 +40,7 @@ const page = () => {
             3. Protection de vos données
           </h3>
           <p className="text-gray-500 text-xs md:text-base font-light w-6/6 mt-5">
-            La sécurité de vos données est primordiale pour nous. trouver-mon-chantier.com
+            La sécurité de vos données est primordiale pour nous. nicolascastera.fr
             utilise des technologies et protocoles sécurisés pour s'assurer que
             vos informations sont bien protégées contre tout accès non autorisé,
             perte, altération ou divulgation. Des mesures telles que le cryptage
@@ -63,7 +63,7 @@ const page = () => {
             5. Modifications de la politique de confidentialité
           </h3>
           <p className="text-gray-500 text-xs md:text-base font-light w-6/6 mt-5">
-            trouver-mon-chantier.com se réserve le droit de modifier cette politique de
+            nicolascastera.fr se réserve le droit de modifier cette politique de
             confidentialité à tout moment. Les modifications entreront en
             vigueur dès leur publication sur notre site. Nous vous encourageons
             donc à consulter régulièrement cette politique afin de rester
