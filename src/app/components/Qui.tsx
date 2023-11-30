a274b179e2fb449d31b6190f32d48cb7c9f8173d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-30 12:40:17 +0100

    Update Qui.tsx

--- src/app/components/Qui.tsx
+++ src/app/components/Qui.tsx
@@ -2,26 +2,25 @@ import React from 'react';
 import Ligne from "./Ligne";
 import { MapPinIcon } from '@heroicons/react/24/solid';
 
-const cards =[
+const cards = [
     {
         title: "Proximité",
-        text: "En tant qu'artisan du web, je m'engage à vous fournir des prospects qualifiés et des projets sur-mesure dans le bâtiment, garantissant une collaboration étroite et sans engagement ni frais de service.",
+        text: "En tant qu'expert en marketing digital, je m'engage à vous fournir des leads qualifiés et des stratégies sur-mesure adaptées à votre secteur, assurant une collaboration étroite et personnalisée, sans engagement ni frais de service."
     },
     {
         title: "Efficacité", 
-        text: "Je maximise l'efficacité de vos campagnes avec des prospects ciblés et des solutions clés en main dans le bâtiment, le tout en tant qu'artisan du web dédié et sans contraintes de service."
+        text: "Je maximise l'efficacité de vos campagnes Google Ads avec des ciblages précis et des solutions clés en main pour divers secteurs d'activité, en tant que spécialiste dédié et sans contraintes de service."
     },
     {
         title: "Rentabilité",
-        text: "Augmentez votre rentabilité grâce à mes services personnalisés dans le bâtiment, offrant des prospects qualifiés et des campagnes efficaces, sans engagement ni frais de service."
+        text: "Augmentez votre rentabilité grâce à mes services de marketing digital personnalisés, offrant des leads qualifiés et des campagnes publicitaires efficaces, sans engagement ni frais de service."
     }
-]
-
+];
 
 const Qui: React.FC = () => {
     return (
-        <section className="" id="qui">
-            <div className="mx-auto flex max-w-5xl p-6 lg:px-8 lg:pt-32 pt-20 md:flex-row flex-col justify-between items-center bg-white">
+        <section className="bg-gray/60" id="qui">
+            <div className="mx-auto flex max-w-5xl p-6 lg:px-8 lg:pt-32 pt-20 md:flex-row flex-col justify-between items-center">
                 <div className="md:w-6/12 w-full text-text">
                     <h2 className="text-2xl font-bold">Qui suis-je ?</h2>
                     <div className="mt-7">
@@ -34,13 +33,13 @@ const Qui: React.FC = () => {
                             <p className='ml-2'>Bordeaux</p>
                         </div>
                     </div>
-                    <p>Expert en marketing digital et communication en ligne, basé à Bordeaux. Fort de plus de 10 ans d'expérience, je me spécialise dans les campagnes Google Ads et la création de landing pages sur-mesure pour les professionnels du bâtiment. Mon approche est centrée sur l'acquisition de prospects qualifiés et l'optimisation des campagnes pour une efficacité maximale. Je m'adapte aux besoins spécifiques de chaque client, offrant une flexibilité budgétaire et une transparence totale. Mon objectif est d'accroître significativement le chiffre d'affaires de mes clients dans le bâtiment, en garantissant des résultats concrets et un taux de conversion élevé, le tout sans engagement ni frais de service.</p>
+<p>Expert en marketing digital et communication en ligne, basé à Bordeaux, je possède plus de 10 ans d'expérience dans l'optimisation de campagnes Google. Ma spécialité réside dans le développement et la gestion de campagnes publicitaires performantes, avec un accent particulier sur l'augmentation des taux de conversion et la génération de résultats mesurables. Je propose des stratégies personnalisées pour chaque client, m'adaptant à leurs objectifs spécifiques tout en offrant flexibilité budgétaire et transparence totale. Mon objectif ultime est d'augmenter significativement le retour sur investissement des campagnes publicitaires, assurant une croissance tangible pour mes clients à travers des stratégies éprouvées et innovantes, le tout avec une approche sans engagement ni frais de service cachés.</p>
                 </div>
                 <div className="md:w-6/12 w-full flex justify-center items-center md:mt-0 mt-12">
                     <img src="/img/moi2.png" className="max-h-[500px] h-auto max-w-[300px]" alt="Image Description" />
                 </div>
             </div>
-            <div className="mx-auto flex max-w-5xl p-6 lg:px-8 lg:pb-32 pb-20 md:flex-row flex-col justify-between items-center bg-white mt-16">
+            <div className="mx-auto flex max-w-5xl p-6 lg:px-8 lg:pb-32 pb-20 md:flex-row flex-col justify-between items-center mt-16">
                 {
                     cards.map((card, index) => (
                         <div className='md:w-4/12 w-full flex flex-col items-center md:max-w-[30%] min-h-[340px] p-7 text-white bg-textSecondary rounded-md md:mt-0 mt-7' key={index}>
