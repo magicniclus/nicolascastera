eecea7641a6a640f7cea09de5fcff603a951d0ee
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-30 16:47:58 +0100

    Update ButtonForm.tsx

--- src/app/components/ButtonForm.tsx
+++ src/app/components/ButtonForm.tsx
@@ -7,12 +7,13 @@ interface ButtonPhoneProps {
     textColor?: string;
     disabled?: boolean;
     type?: "submit" | "button" | "reset" | undefined;
+    id?: string;
 }
 
-const ButtonForm : React.FC<ButtonPhoneProps> = ({ value = 'Envoyer', bgColor = "textSecondary", textColor = "white" , disabled= true, type="submit"}) => {
+const ButtonForm : React.FC<ButtonPhoneProps> = ({ value = 'Envoyer', bgColor = "textSecondary", textColor = "white" , disabled= true, type="submit", id}) => {
 
     return (
-        <button type={type} disabled={disabled} className={`py-3 lg:px-5 px-3 rounded-lg bg-${bgColor} text-${textColor} flex mt-10 min-w-[150px] justify-center items-center hover:transition-all hover:duration-100 hover:ease-in-out ${disabled ? "opacity-80": "opacity-100 hover:shadow-lg"}`}>
+        <button id={id} type={type} disabled={disabled} className={`py-3 lg:px-5 px-3 rounded-lg bg-${bgColor} text-${textColor} flex mt-10 min-w-[150px] justify-center items-center hover:transition-all hover:duration-100 hover:ease-in-out ${disabled ? "opacity-80": "opacity-100 hover:shadow-lg"}`}>
             <div className='flex'>
                 {value}
             </div>
