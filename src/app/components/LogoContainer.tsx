3a43210e7f63b42ad44b649c97d4a3b837f5f370
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-30 12:14:02 +0100

    Update LogoContainer.tsx

--- src/app/components/LogoContainer.tsx
+++ src/app/components/LogoContainer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logos from './logos';
+import Logos from "./logos";
 
 // Définir l'interface pour un élément de data
 interface DataItem {
@@ -15,9 +15,9 @@ interface Props {
 const LogoContainer : React.FC<Props> = ({data}) => {
     return (
         <section>
-            <div className='mx-auto flex max-w-5xl px-6 lg:pb-32 pb-20 pt-14 lg:px-8 flex-col justify-center items-center text-text'>
-                <h2 className="text-2xl font-bold text-center">Une maitrise total de l'ensemble des outils Google</h2>
-                <div className='flex flex-row flex-wrap'>
+            <div className='mx-auto flex max-w-5xl  p-6 lg:px-8 lg:py-32 py-20 flex-col justify-center items-center text-text'>
+                <h2 className="text-2xl font-bold text-center mb-5">Une maitrise total de l'ensemble des outils Google</h2>
+                <div className='flex flex-wrap justify-around'>
                     {
                         data && data.map((item: any, index: number) => (
                             <Logos key={index} title={item.title} img={item.img} text={item.text} />
