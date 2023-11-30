27c2c896912e90a6403111672ae337151a013da4
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-30 10:27:19 +0100

    Create LittleCards.tsx

---
+++ src/app/components/LittleCards.tsx
@@ -0,0 +1,17 @@
+import { CheckCircleIcon } from '@heroicons/react/24/solid';
+import React from 'react';
+
+interface LittleCardsProps {
+    value: string;
+}
+
+const LittleCards : React.FC<LittleCardsProps> = ({value}) => {
+    return (
+        <div className='flex mt-5 p-3 rounded-full bg-gray/60 max-w-max min-w-[270px]'>
+            <CheckCircleIcon className='h-5 w-5 text-yellow' />
+            <p className='ml-3'>{value}</p>
+        </div>
+    );
+};
+
+export default LittleCards;
