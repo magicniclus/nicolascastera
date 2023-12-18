2e0d639dee6ae425b73c58796e056f81c41ca4ff
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-12-18 09:51:49 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import LogoContainer from './components/LogoContainer';
 import CTA from './components/CTA';
+import ServiceLittleBanner from './components/ServiceLittleBanner';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -86,6 +87,7 @@ export default function Home() {
       <Hero/>
       </div>
       <ImgWithText  imgUrl="/img/resultat.png" title="Devenez maitre de votre rentabilité" paragraphs={[ <p  key="paragraph1">Grace à Google, ciblez votre client idéal en fonction de son comportement de recherche, son historique de recherche et sa zone géographique. Vous vous positionné devant les bonnes personnes afin d'augmenter vos chance de conversion.</p>, <p  key="paragraph4"><span className="font-bold">Suivi et Développement :</span> J'optimise en continu vos campagnes en ajustant les mots clés, les enchaires, les audiences ou enore les annonces.</p>, <p key="paragraph2"><span className="font-bold">Flexibilité Budgétaire :</span> Ajustez votre budget en continu pour l'aligner avec vos objectifs commerciaux et les variations saisonnières de votre activité.</p>, <p key="paragraph3"><span className="font-bold">Transparence Totale :</span> Grâce aux notifications instantanées des ventes réalisées, des appels et des formulaires générés par la campagne, ainsi qu'à un reporting dédié, vous avez une vision claire et précise de la rentabilité de votre investissement. Cette transparence vous permet de prendre des décisions éclairées pour optimiser vos stratégies marketing.</p> ]} />
+      <ServiceLittleBanner />
       <div id="services">
         <LogoContainer data={googleCards} />
       </div>
