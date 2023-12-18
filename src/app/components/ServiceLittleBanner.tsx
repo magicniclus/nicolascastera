604082fb9ead3d115d4be887421539499b9940db
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-12-18 09:51:47 +0100

    Create ServiceLittleBanner.tsx

---
+++ src/app/components/ServiceLittleBanner.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const ServiceLittleBanner = () => {
+    return (
+        <section className='bg-gray/40'>
+            <div className='mx-auto flex max-w-5xl p-6 lg:px-8 lg:py-10 md:flex-row flex-col justify-between items-center md:items-center'>
+                <ul className='flex w-full justify-between items-center text-textSecondary md:flex-row flex-col'>
+                    <li>
+                        <h2 className='text-2xl font-bold md:mt-0 mt-3'>💶 E-Commerce</h2>
+                    </li>
+                    <li>
+                        <p className='text-2xl font-bold md:mt-0 mt-3'>💻 Aquisition de lead</p>
+                    </li>
+                    <li>
+                        <p className='text-2xl font-bold md:mt-0 mt-3'>📱 Vente d'application</p>
+                    </li>
+                    <li>
+                        <p className='text-2xl font-bold md:mt-0 mt-3'>✨ Notoriété</p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    );
+};
+
+export default ServiceLittleBanner;
