f44fe92cb20aadacdc87225d6da0acd5427a16ef
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-12-18 16:29:36 +0100

    Update NavBar.tsx

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -12,11 +12,12 @@ const NavBar = () => {
     return (
         <header className='text-text'>
             <nav className="mx-auto flex max-w-5xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-                <div className="flex">
-                    <a href="/" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Trouver-mon-chantier</span>
-                        <img className="h-14 w-auto" src="/logo/logo.png" alt="trouver-mon-chantier" />
+                <div className="flex items-top">
+                    <a href="/" className="">
+                        <span className="sr-only">Nicolas CASTERA</span>
+                        <img className="h-14 w-auto" src="/logo/logo.png" alt="logo Nicolas CASTERA" />
                     </a>
+                    <img className="h-14 w-auto" src="/logo/certifica.png" alt="logo Certifié Google" />
                 </div>
                 <div className='md:block hidden'>
                     {navigation.map((item, id) => (
